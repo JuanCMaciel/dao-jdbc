d649182d076b25df43af300c4d0313699b4391eb
JuanCMaciel <macielcjuan@gmail.com>
2023-03-07 16:28:55 -0300

    DepartmentDaoJDBC implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,16 +1,20 @@
 package application;
 
+import java.util.Date;
+import java.util.List;
 import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		/*
+		
 		System.out.println("=== TEST 1: seller findById ===");
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
@@ -38,7 +42,7 @@ public class Program {
 		seller.setName("Martha Waine");
 		sellerDao.update(seller);
 		System.out.println("Update completed");
-		*/
+		
 		
 		Scanner sc = new Scanner(System.in);
 		System.out.println("\n=== TEST 6: seller delete ===");
